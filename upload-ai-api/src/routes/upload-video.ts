f5ebdc24327fe8263c46660340d5f620c1e8c5a3
vicenttcarvalho <vicentecleyton@gmail.com>
2023-09-18 10:03:52 -0300

    conversion error correction

--- upload-ai-api/src/routes/upload-video.ts
+++ upload-ai-api/src/routes/upload-video.ts
@@ -24,7 +24,7 @@ export async function uploadVideoRoute(app: FastifyInstance) {
 
     const extension = path.extname(data.filename);
 
-    if (extension != ".mp3") {
+    if (extension !== ".mp3") {
       return reply
         .status(400)
         .send({ error: "Invalid input type, please upload a MP3" });
